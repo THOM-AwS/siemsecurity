f7231990890ca52241b5f49fedd67006fa4c8ae4
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-24 16:51:31 +1100

    logging

--- main.tf
+++ main.tf
@@ -126,6 +126,26 @@ resource "aws_iam_role" "ecs_execution_role" {
   })
 }
 
+resource "aws_iam_role_policy" "ecs_execution_role_policy" {
+  name   = "ecs_execution_role_policy"
+  role   = aws_iam_role.ecs_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents"
+        ],
+        Effect = "Allow",
+        Resource = "arn:aws:logs:us-east-1:941133421128:log-group:/ecs/siem:*"
+      },
+    ]
+  })
+}
+
 # Task definition for Grafana
 resource "aws_ecs_task_definition" "grafana" {
   family                   = "grafana"
