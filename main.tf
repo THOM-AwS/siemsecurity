1d4102c5e30992fce57679821e3e9d523e5981f9
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-24 16:53:20 +1100

    lint

--- main.tf
+++ main.tf
@@ -127,8 +127,8 @@ resource "aws_iam_role" "ecs_execution_role" {
 }
 
 resource "aws_iam_role_policy" "ecs_execution_role_policy" {
-  name   = "ecs_execution_role_policy"
-  role   = aws_iam_role.ecs_execution_role.id
+  name = "ecs_execution_role_policy"
+  role = aws_iam_role.ecs_execution_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -139,7 +139,7 @@ resource "aws_iam_role_policy" "ecs_execution_role_policy" {
           "logs:CreateLogGroup",
           "logs:PutLogEvents"
         ],
-        Effect = "Allow",
+        Effect   = "Allow",
         Resource = "arn:aws:logs:us-east-1:941133421128:log-group:/ecs/siem:*"
       },
     ]
