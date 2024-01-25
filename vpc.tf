dbe157ef5529113f3d4f841274e233d89fc7965e
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-26 10:02:28 +1100

    stop

--- vpc.tf
+++ vpc.tf
@@ -1,92 +1,92 @@
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
+# # Create a VPC
+# resource "aws_vpc" "main" {
+#   cidr_block           = "10.0.0.0/16"
+#   enable_dns_support   = true
+#   enable_dns_hostnames = true
+# }
 
-# Internet Gateway for the public subnets
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
+# # Internet Gateway for the public subnets
+# resource "aws_internet_gateway" "gw" {
+#   vpc_id = aws_vpc.main.id
+# }
 
-# Public Subnets
-resource "aws_subnet" "public1" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
-}
+# # Public Subnets
+# resource "aws_subnet" "public1" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.1.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "us-east-1a"
+# }
 
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "us-east-1b"
-}
+# resource "aws_subnet" "public2" {
+#   vpc_id                  = aws_vpc.main.id
+#   cidr_block              = "10.0.2.0/24"
+#   map_public_ip_on_launch = true
+#   availability_zone       = "us-east-1b"
+# }
 
-# Private Subnets
-resource "aws_subnet" "private1" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
-  availability_zone = "us-east-1a"
-}
+# # Private Subnets
+# resource "aws_subnet" "private1" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.3.0/24"
+#   availability_zone = "us-east-1a"
+# }
 
-resource "aws_subnet" "private2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.4.0/24"
-  availability_zone = "us-east-1b"
-}
+# resource "aws_subnet" "private2" {
+#   vpc_id            = aws_vpc.main.id
+#   cidr_block        = "10.0.4.0/24"
+#   availability_zone = "us-east-1b"
+# }
 
-# NAT Gateway for private subnets
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
-}
+# # NAT Gateway for private subnets
+# resource "aws_eip" "nat_eip" {
+#   domain = "vpc"
+# }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public1.id
-}
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.public1.id
+# }
 
-# Public Route Table
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+# # Public Route Table
+# resource "aws_route_table" "public" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.gw.id
+#   }
+# }
 
-# Associate Public Route Table with public subnets
-resource "aws_route_table_association" "public1" {
-  subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public.id
-}
+# # Associate Public Route Table with public subnets
+# resource "aws_route_table_association" "public1" {
+#   subnet_id      = aws_subnet.public1.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-resource "aws_route_table_association" "public2" {
-  subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public.id
-}
+# resource "aws_route_table_association" "public2" {
+#   subnet_id      = aws_subnet.public2.id
+#   route_table_id = aws_route_table.public.id
+# }
 
-# Private Route Table
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+# # Private Route Table
+# resource "aws_route_table" "private" {
+#   vpc_id = aws_vpc.main.id
 
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-}
+#   route {
+#     cidr_block     = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.nat.id
+#   }
+# }
 
-# Associate Private Route Table with private subnets
-resource "aws_route_table_association" "private1" {
-  subnet_id      = aws_subnet.private1.id
-  route_table_id = aws_route_table.private.id
-}
+# # Associate Private Route Table with private subnets
+# resource "aws_route_table_association" "private1" {
+#   subnet_id      = aws_subnet.private1.id
+#   route_table_id = aws_route_table.private.id
+# }
 
-resource "aws_route_table_association" "private2" {
-  subnet_id      = aws_subnet.private2.id
-  route_table_id = aws_route_table.private.id
-}
+# resource "aws_route_table_association" "private2" {
+#   subnet_id      = aws_subnet.private2.id
+#   route_table_id = aws_route_table.private.id
+# }
