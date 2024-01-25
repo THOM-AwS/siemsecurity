e09b5aa2a54a3e48d391c33d683cebd7a1bce5fc
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-26 10:30:48 +1100

    start

--- securityGroups.tf
+++ securityGroups.tf
@@ -1,22 +1,22 @@
 
 
-# # Security group default all
-# resource "aws_security_group" "all" {
-#   vpc_id = aws_vpc.main.id
+# Security group default all
+resource "aws_security_group" "all" {
+  vpc_id = aws_vpc.main.id
 
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   ingress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   tags = {
-#     Name = "open-all"
-#   }
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "open-all"
+  }
+}
