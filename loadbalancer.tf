ad9578ef3e3bff43130c2260e98d7d3cf1b8dee7
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-26 11:31:21 +1100

    lb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -11,16 +11,16 @@ resource "aws_lb" "grafana_alb" {
   enable_deletion_protection = false
 }
 
-resource "aws_lb_listener" "grafana_listener" {
-  load_balancer_arn = aws_lb.grafana_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
+# resource "aws_lb_listener" "grafana_listener" {
+#   load_balancer_arn = aws_lb.grafana_alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana_tg.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.grafana_tg.arn
+#   }
+# }
 
 resource "aws_lb_target_group" "grafana_tg" {
   name        = "grafana-tg"
