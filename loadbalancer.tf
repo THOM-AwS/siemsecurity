7afe1aecdae20be1b00218456298b74a94014885
Thomas Hamer <thomas_hamer@outlook.com>
2024-01-27 14:20:36 +1100

    update-alb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,7 +1,7 @@
 
 
 # Application Load Balancer, Listener, and Target Group for Grafana
-resource "aws_lb" "grafana_alb" {
+resource "aws_lb" "soc_alb" {
   name               = "grafana-alb"
   internal           = false
   load_balancer_type = "application"
@@ -12,7 +12,7 @@ resource "aws_lb" "grafana_alb" {
 }
 
 resource "aws_lb_listener" "grafana_listener" {
-  load_balancer_arn = aws_lb.grafana_alb.arn
+  load_balancer_arn = aws_lb.soc_alb.arn
   port              = "80"
   protocol          = "HTTP"
 
@@ -45,4 +45,51 @@ resource "aws_lb_target_group_attachment" "grafana_attachment" {
   target_group_arn = aws_lb_target_group.grafana_tg.arn
   target_id        = module.ec2_grafana.id
   port             = 3000
+}
+
+
+
+
+
+
+
+# Wazuh Dashboard Target Group
+resource "aws_lb_target_group" "wazuh_tg" {
+  name        = "wazuh-tg"
+  port        = 5601
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    matcher             = "200-299"
+  }
+}
+
+# Wazuh Dashboard Target Group Attachment
+resource "aws_lb_target_group_attachment" "wazuh_attachment" {
+  target_group_arn = aws_lb_target_group.wazuh_tg.arn
+  target_id        = module.ec2_wazuh.id
+  port             = 5601
+}
+
+# Listener for Wazuh Dashboard
+resource "aws_lb_listener" "wazuh_listener" {
+  load_balancer_arn = aws_lb.soc_alb.arn
+  port              = "81"
+  protocol          = "HTTP"
+
+  # Assuming you want to route traffic to the Wazuh dashboard based on path
+  # Update this rule as necessary based on your desired routing criteria
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.wazuh_tg.arn
+  }
 }
