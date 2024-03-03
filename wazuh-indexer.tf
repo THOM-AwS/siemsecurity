d39dfed68813dae0fc90aade54b98c23ca40143a
Thomas Hamer <thomas_hamer@outlook.com>
2024-03-03 21:33:18 +1100

    update userdata

--- wazuh-indexer.tf
+++ wazuh-indexer.tf
@@ -38,7 +38,8 @@ NODE_NAME=$(hostname)
 
 # Install and configure Let's Encrypt certbot
 yum install -y certbot python3-certbot-dns-route53
-certbot certonly --dns-route53 -d 127cyber.com -d wazuh.127cyber.com -d listen.127cyber.com --non-interactive --agree-tos -m thomas_hamer@outlook.com
+certbot certonly --dns-route53 -d 127cyber.com -d wazuh.127cyber.com -d listen.127cyber.com --non-interactive --agree-tos -m thomas_hamer@outlook.com --server https://acme-v02.api.letsencrypt.org/directory --cert-name 127cyber.com
+certbot certonly --dns-route53 -d admin.127cyber.com --non-interactive --agree-tos -m thomas_hamer@outlook.com --server https://acme-v02.api.letsencrypt.org/directory --cert-name admin.127cyber.com
 (crontab -l 2>/dev/null; echo "0 3 * * * /usr/bin/certbot renew --quiet") | crontab -
 
 # Install Wazuh indexer and manager
@@ -56,9 +57,11 @@ yum -y install wazuh-indexer wazuh-manager wazuh-dashboard
 
 # Node configuration
 mkdir -p /etc/wazuh-indexer/certs
+cp /etc/letsencrypt/live/admin.127cyber.com/fullchain.pem /etc/wazuh-indexer/certs/admin.pem
+cp /etc/letsencrypt/live/admin.127cyber.com/privkey.pem /etc/wazuh-indexer/certs/admin-key.pem
 cp /etc/letsencrypt/live/127cyber.com/fullchain.pem /etc/wazuh-indexer/certs/indexer.pem
 cp /etc/letsencrypt/live/127cyber.com/privkey.pem /etc/wazuh-indexer/certs/indexer-key.pem
-awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/' /etc/letsencrypt/live/127cyber.com/fullchain.pem > /etc/wazuh-indexer/certs/root-ca.pem
+cat /etc/letsencrypt/live/127cyber.com/fullchain.pem > /etc/wazuh-indexer/certs/root-ca.pem
 chmod 500 /etc/wazuh-indexer/certs
 chmod 444 /etc/wazuh-indexer/certs/*
 chown -R wazuh-indexer:wazuh-indexer /etc/wazuh-indexer/certs
@@ -69,7 +72,7 @@ sed -i 's/-Xmx1g/-Xmx4g/g' /etc/wazuh-indexer/jvm.options
 mkdir -p /etc/wazuh-dashboard/certs
 cp /etc/letsencrypt/live/127cyber.com/fullchain.pem /etc/wazuh-dashboard/certs/dashboard.pem
 cp /etc/letsencrypt/live/127cyber.com/privkey.pem /etc/wazuh-dashboard/certs/dashboard-key.pem
-awk '/BEGIN CERTIFICATE/,/END CERTIFICATE/' /etc/letsencrypt/live/127cyber.com/fullchain.pem > /etc/wazuh-dashboard/certs/root-ca.pem
+cat /etc/letsencrypt/live/127cyber.com/fullchain.pem > /etc/wazuh-dashboard/certs/root-ca.pem
 chmod 500 /etc/wazuh-dashboard/certs
 chmod 444 /etc/wazuh-dashboard/certs/*
 chown -R wazuh-dashboard:wazuh-dashboard /etc/wazuh-dashboard/certs
@@ -78,10 +81,10 @@ chown -R wazuh-dashboard:wazuh-dashboard /etc/wazuh-dashboard/certs
 sed -i "s/NODE_IP/$INSTANCE_IP/g" /var/ossec/etc/ossec.conf
 
 # /etc/wazuh-indexer/opensearch.yml
-sed -i "s/"node-1"/"$NODE_NAME"/g" /etc/wazuh-indexer/opensearch.yml
 sed -i "s/wazuh-cluster/127cyber/g" /etc/wazuh-indexer/opensearch.yml
 sed -i "s/#discovery.seed_hosts:/discovery.seed_hosts:/g" /etc/wazuh-indexer/opensearch.yml
-sed -i "s/#  - \"node-1-ip\"/ - $INSTANCE_IP/g" /etc/wazuh-indexer/opensearch.yml
+sed -i "s/#  - \"node-1-ip\"/ - \"$INSTANCE_IP\"/g" /etc/wazuh-indexer/opensearch.yml
+sed -i "s/\"node-1\"/\"$NODE_NAME\"/g" /etc/wazuh-indexer/opensearch.yml
 echo -e "\nbootstrap.memory_lock: true" | sudo tee -a /etc/wazuh-indexer/opensearch.yml
 sed -i "s/CN=admin,OU=Wazuh,O=Wazuh,L=California,C=US/CN=127cyber.com/g" /etc/wazuh-indexer/opensearch.yml
 sed -i "s/CN=,OU=Wazuh,O=Wazuh,L=California,C=US/CN=127cyber.com/g" /etc/wazuh-indexer/opensearch.yml
@@ -103,6 +106,6 @@ systemctl start wazuh-indexer
 systemctl start wazuh-manager
 systemctl start wazuh-dashboard
 
-JAVA_HOME=/usr/share/wazuh-indexer/jdk runuser wazuh-indexer --shell=/bin/bash --command="cd /etc/wazuh-indexer/opensearch-security && /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cacert /etc/wazuh-indexer/certs/root-ca.pem -cert /etc/wazuh-indexer/certs/indexer.pem -key /etc/wazuh-indexer/certs/indexer-key.pem -h 127.0.0.1 -p 9200 -icl -nhnv"
+JAVA_HOME=/usr/share/wazuh-indexer/jdk runuser wazuh-indexer --shell=/bin/bash --command="cd /etc/wazuh-indexer/opensearch-security && /usr/share/wazuh-indexer/plugins/opensearch-security/tools/securityadmin.sh -cacert /etc/wazuh-indexer/certs/root-ca.pem -cert /etc/wazuh-indexer/certs/admin.pem -key /etc/wazuh-indexer/certs/admin-key.pem -h 127.0.0.1 -p 9200 -icl -nhnv"
   TEOF
 }
